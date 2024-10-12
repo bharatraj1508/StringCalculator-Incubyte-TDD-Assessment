ec713debdd8c61b044062eb3348066fa3d552708
Bharat Raj Verma <bharat.raj1508@gmail.com>
2024-10-12 21:31:26 +0530

    Test case 4. No need to refactor code as previous solution works. Part 2 of assessment completed

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -27,6 +27,18 @@ function testForTwoNumbers() {
   );
 }
 
+// PART 2: MULTIPLE NUMBERS
+
+//Test Case 4: For more than two numbers string
+function tesetForMultipleNumbers() {
+  const result = add("4,5,2,4,8");
+  console.assert(result === 23, `Expected result 23 but got ${result}`);
+  console.log(
+    result === 23 ? "Test case 4 passed for multiple numbers" : "Test 4 failed"
+  );
+}
+
 testForEmptyString();
 testForOneNumber();
 testForTwoNumbers();
+tesetForMultipleNumbers();
