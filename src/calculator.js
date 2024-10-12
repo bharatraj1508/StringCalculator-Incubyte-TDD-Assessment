7ccc6c702de820bb201fef4e96d33427fa88fbe5
Bharat Raj Verma <bharat.raj1508@gmail.com>
2024-10-12 21:58:48 +0530

    Refactored code for test case 5 and 6

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,7 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  else if (numbers.includes(",")) {
-    let numArray = numbers.split(",");
+  else if (numbers.includes(",") || numbers.includes("\n")) {
+    let numArray = numbers.split(/[\n,]/);
     return numArray.reduce((sum, num) => sum + parseInt(num), 0);
   } else {
     return parseInt(numbers);
