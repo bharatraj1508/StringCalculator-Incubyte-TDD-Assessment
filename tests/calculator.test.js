cecb3b8e7f054eb2a889efe2b30157a06fae69c5
Bharat Raj Verma <bharat.raj1508@gmail.com>
2024-10-12 20:55:55 +0530

    Test 2 for single number

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -4,7 +4,19 @@ const { add } = require("../src/calculator");
 function testForEmptyString() {
   const result = add("");
   console.assert(result === 0, `Expected result 0 but got ${result}`);
-  console.log("Test 1 passed for empty string ");
+  console.log(
+    result === 0 ? "Test case 2 passed for empty string" : "Test 1 failed"
+  );
+}
+
+//Test Case 2: For one number string
+function testForOneNumber() {
+  const result = add("2");
+  console.assert(result === 2, `Expected result 2 but got ${result}`);
+  console.log(
+    result === 2 ? "Test case 2 passed for single number" : "Test 2 failed"
+  );
 }
 
 testForEmptyString();
+testForOneNumber();
